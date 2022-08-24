464d2c50a98215382254eaafca7066eed8348a9b
turgut yılmaz <songulturgut0641@gmail.com>
2022-08-24 13:34:41 +0300

    usestate

---
+++ s05_events-useStateHook/src/App.js
@@ -0,0 +1,15 @@
+import Events from './components/1-event-intro/Events';
+import Counter from './components/2-classComponents/Counter';
+import UseStateExample from './components/3-useStateHook/UseStateExample';
+
+function App() {
+  return (
+    <div>
+      <Events />
+      <Counter count={10}/>
+      <Counter />
+      <UseStateExample/>
+    </div>
+  );
+}
+export default App;
